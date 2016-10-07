c22bdb82d1dcdc4f90db761d3150e873fcafafd4
gustaf-kingq <gvmw@live.com>
2016-10-07 14:24:32 +0200

    1007 - Experience Model/List Notable changes: - Added the experience model. - Faked the connection with the model and the contructor. - Added the creation of a experiencelist on the controller.

--- Demo10071/Controllers/HomeController.cs
+++ Demo10071/Controllers/HomeController.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+//Adding a class up here fakes a connection, or more like establishing one.
+using Demo10071.Models;
 
 namespace Demo10071.Controllers
 {
     public class HomeController : Controller
     {
+        //Fakes a connection to the Database for experiences
+        List<Experience>
+
+            private List<Experience> MyExperienceList;
+            public HomeController()
+        {
+            MyExperienceList = new List<Experience>();
+            MyExperienceList.Add(new Experience() { Title = "", Description = "", Period = "" });
+        }
+
         // GET: Home
         public ActionResult Index()
         {
