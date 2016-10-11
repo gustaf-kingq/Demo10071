3e7c2fa4a5beb3a42f07d95162d63ab7e90b4140
gustaf-kingq <gvmw@live.com>
2016-10-11 14:43:31 +0200

    1011 - Added Database Notable changes: - Added a database prior to demo 1011.

--- Demo10071/Controllers/HomeController.cs
+++ Demo10071/Controllers/HomeController.cs
@@ -3,22 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-//Adding a class up here fakes a connection, or more like establishing one.
 using Demo10071.Models;
 
 namespace Demo10071.Controllers
 {
     public class HomeController : Controller
     {
-        //Fakes a connection to the Database for experiences
-        List<Experience>
+        //Fejkar koppling till en DB för Experiences
 
-            private List<Experience> MyExperienceList;
-            public HomeController()
+        private List<Experience> MyExperienceList;
+
+        public HomeController()
         {
             MyExperienceList = new List<Experience>();
-            MyExperienceList.Add(new Experience() { Title = "", Description = "", Period = "" });
+            MyExperienceList.Add(new Experience() { Title = "Målare", Description = "Målade farfars hus", Period = "2016" });
+            MyExperienceList.Add(new Experience() { Title = "Tryckare", Description = "Tryckte tidningar", Period = "2015-" });
+            MyExperienceList.Add(new Experience() { Title = "Försäljare", Description = "Sålde postkodlotter", Period = "2015" });
         }
+
+        //===========================================
 
         // GET: Home
         public ActionResult Index()
@@ -28,7 +31,7 @@ namespace Demo10071.Controllers
 
         public ActionResult About()
         {
-            return View();
+            return View(MyExperienceList);
         }
     }
 }
